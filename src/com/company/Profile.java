4bd4b6d795233ad6655f87919cc417eb9bc3bb06
serg-qa <sergio.molchanov88@gmail.com>
2017-10-02 15:42:41 +0300

    release

---
+++ src/com/company/Profile.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.io.*;
+
+public class Profile {
+
+    // первоначальные проверки;
+
+    public static void checkFile(File sourceFile){
+        System.out.println("Имя файла, что копируем: " + sourceFile.getName() + ";");
+        System.out.println("Расположение файла: " + sourceFile.getParent() + ";");
+
+        if (sourceFile.exists())
+            System.out.println(sourceFile.getName() + ": существует;");
+        else
+            System.out.println(sourceFile.getName() + ": не существует;");
+
+        System.out.println("Размер копируемого файла: " + sourceFile.length() + " байт;");
+        if (sourceFile.canRead())
+            System.out.println(sourceFile.getName() + ": доступен для чтения;");
+        else
+            System.out.println(sourceFile.getName() + " : не доступен для чтения;");
+
+        if (sourceFile.canWrite())
+            System.out.println(sourceFile.getName() + ": доступен для записи;");
+        else
+            System.out.println(sourceFile.getName() + ": доступен для записи;");
+    }
+
+    // построчная проверка исходного файла с копией;
+
+    public static void comparisonFile(File source, File newFile){
+        try {
+
+            FileReader frSource = new FileReader(source);
+            FileReader frNew = new FileReader(newFile);
+
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader readerSource = new BufferedReader(frSource);
+            BufferedReader readerNew = new BufferedReader(frNew);
+
+            // считаем сначала первую строку
+            String lineNew = "";
+            String lineSource = "";
+
+            while (lineSource != null) {
+
+                // считываем остальные строки в цикле
+                lineSource = readerSource.readLine();
+                lineNew = readerNew.readLine();
+
+                if (lineSource == null && lineNew == null) {
+                    break;
+                }else {
+                    if (lineSource.equals(lineNew)) {
+                    } else {
+                        System.out.println("Строка не совпадает: " + lineNew);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Построчное сравнение фалов успешно выполнено.");
+    }
+
+}
